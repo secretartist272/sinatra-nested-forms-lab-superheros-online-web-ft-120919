37422c0839eff849478bc6645f3e8f482f1b4b26
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-26 02:34:53 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,25 +1,3 @@
 class SuperHero 
-  attr_accessor :name, :power, :biography
-
-  @@all = []
-
-  def initialize(attributes)
-    @name = attributes[:name]
-    @power = attributes[:power]
-    @biography = attributes[:biography]
-    self.save
-  end
-
-  def save
-    @@all << self
-  end
-
-  def all
-    @@all
-  end
-
-  def clear
-    @@all = []
-  end
-
+  
 end

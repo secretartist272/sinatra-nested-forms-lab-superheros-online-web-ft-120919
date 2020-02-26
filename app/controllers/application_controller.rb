2b99ebb99e8a54cc0ca1d480aac32d1fe3840c7b
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-26 02:37:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
     @hero_name = []
     @hero_power = []
     @hero_bio = []
-    @team_members = params[:team][:members]
+    @team_members = params[:team][:heroes]
     @team_members.each do |hero|
       @hero_name << hero[:name]
       @hero_power << hero[:power]

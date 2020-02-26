ce07d2b0d06241c8d0d54b7b4a8475d5ce6e5103
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-26 02:38:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,18 @@ class App < Sinatra::Base
   end
   
   post '/teams' do
-    @team = params[:team][:name]
-    @team_motto = params[:team][:motto]
-    @hero_name = []
-    @hero_power = []
-    @hero_bio = []
-    @team_members = params[:team][:heroes]
-    @team_members.each do |hero|
-      @hero_name << hero[:name]
-      @hero_power << hero[:power]
-      @hero_bio << hero[:bio]
-    end
+    @team = Team.new(params[:team][:name], params[:team][:motto])
 
+      @hero_1 = SuperHero.new(params[:team][:heroes][0][:name], params[:team][:heroes][0][:power], params[:team][:heroes][0][:bio])
+      @hero_2 = SuperHero.new(params[:team][:heroes][1][:name], params[:team][:heroes][1][:power], params[:team][:heroes][1][:bio])
+      @hero_3 = SuperHero.new(params[:team][:heroes][2][:name], params[:team][:heroes][2][:power], params[:team][:heroes][2][:bio])
+      #binding.pry
+
+      @team.add_hero(@hero_1)
+      @team.add_hero(@hero_2)
+      @team.add_hero(@hero_3)
+
+      @heroes = @team.heroes 
     erb :team
   end    
 end
